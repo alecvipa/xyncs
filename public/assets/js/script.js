5d93daff58e1269d36830aed35564cbbcd82d519
alecvipa <hebrit_626@hotmail.com>
2021-06-11 20:01:01 -0500

    added function contacto

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -48,3 +48,6 @@ $(".goToContact").on('click', function () {
 
 //     // Proving FB
 // });
+$(".bannerDiving, .bannerDivingInsta").click(function(){
+    window.open("https://www.xyncs.com/contacto");
+});
